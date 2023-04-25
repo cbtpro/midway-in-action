15b6e4b2c427536c857a81d6a24adc2de2ead6a9
chenbitao <cbtpro@163.com>
2023-04-25 17:45:13 +0800

    feat: 获取天气的功能

--- src/controller/weather.controller.ts
+++ src/controller/weather.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Get } from '@midwayjs/core';
+import { Controller, Get, Inject, Query } from '@midwayjs/core';
+import { WeatherService } from '../service/weather.service';
+import { IWeatherInfo } from '../interface';
 
 @Controller('/')
 export class WeatherController {
+  @Inject()
+  weatherController: WeatherService;
   // 这是一个装饰器，定义一个路由
+  // 访问/weather?cityId=101010100来测试接口
   @Get('/weather')
-  async getWeatherInfo(): Promise<string> {
-    return 'Hello Weather';
+  async getWeatherInfo(@Query('id') cityId: string): Promise<IWeatherInfo> {
+    return this.weatherController.getWeather(cityId);
   }
 }
--- src/interface.ts
+++ src/interface.ts
@@ -4,3 +4,21 @@
 export interface IUserOptions {
   uid: number;
 }
+
+export interface IWeatherInfo {
+  weatherInfo: {
+    city: string;
+    cityId: string;
+    temp: string;
+    WD: string;
+    WS: string;
+    SD: string;
+    AP: string;
+    njd: string;
+    WSE: string;
+    time: string;
+    sm: string;
+    isRadar: string;
+    Radar: string;
+  };
+}
